e613a3cd4303697d2d550834e9a31abf8d58c095
James Holding <james.holding90@gmail.com>
2020-06-09 23:00:50 -0400

    timer added. Basic functionality only.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 var startGameBtnEl = document.getElementById("start-button");
 var startPageEl = document.getElementById("start-page");
+var timeEl = document.querySelector("time");
 
-startGameBtnEl.addEventListener("click", function (event) {
+startGameBtnEl.addEventListener("click", function () {
   console.log("Button Clicked.");
-  if ("click") {
-    startPageEl.style.display = "none";
-  } else {
-    return;
-  }
+  startPageEl.style.display = "none";
+  timeRemain();
 });
+
+// function startQuiz(event) {
+//   if (event.target.matches("#click")) {
+//     startPageEl.style.display = "none";
+//   } else {
+//     startPageEl.style.display = "block";
+//   }
+// }
+
+function timeRemain() {
+  var timeLeft = 60;
+  var timeInterval = setInterval(function () {
+    time.textContent = "Time: " + timeLeft;
+    timeLeft--;
+  }, 1000);
+}
