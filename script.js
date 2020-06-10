37d56cd6d26fcfac83cc6691a41380eec1c6d648
James Holding <james.holding90@gmail.com>
2020-06-10 00:20:32 -0400

    Added in feature to clear start page and add in question.

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 var startGameBtnEl = document.getElementById("start-button");
 var startPageEl = document.getElementById("start-page");
 var timeEl = document.getElementById("time");
+var cardTitle = document.querySelector(".card-title");
+var cardText = document.querySelector(".card-text");
 
+
+// Start the game and begin the time. 
 startGameBtnEl.addEventListener("click", function () {
-  console.log("Button Clicked.");
-  startPageEl.style.display = "none";
+  // console.log("Button Clicked.");
+  // startPageEl.style.display = "none";
   timeRemain();
+  renderQuestion();
 });
 
+
+function renderQuestion() {
+  // Clear todoList element and update todoCountSpan
+  cardTitle.innerHTML = "Insert Function for Question Title";
+  cardText.textContent = "Insert Funtion for Question Answers";
+  startGameBtnEl.remove()
+}
+
 // function startQuiz(event) {
-//   if (event.target.matches("#click")) {
+//   if (event.target.matches(".btn")) {
 //     startPageEl.style.display = "none";
 //   } else {
 //     startPageEl.style.display = "block";
 //   }
 // }
 
+// Timer function 
 function timeRemain() {
   var timeLeft = 60;
   var timeInterval = setInterval(function () {
     timeEl.textContent = "Time: " + timeLeft;
     timeLeft--;
+    console.log("Timer Started");
     if (timeLeft === -1) {
       clearInterval(timeInterval);
     }
