445b2dd01d737e4479b366be2c31f36dea9de36d
James Holding <james.holding90@gmail.com>
2020-06-10 01:27:19 -0400

    prototype way of adding answers in.

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ var startPageEl = document.getElementById("start-page");
 var timeEl = document.getElementById("time");
 var cardTitle = document.querySelector(".card-title");
 var cardText = document.querySelector(".card-text");
+var listGroup = document.querySelector(".list-group");
 
+var question = ["Commonly used data types DO NOT include:"];
+var answers = ["strings", "booleans", "alerts", "numbers"];
 
-// Start the game and begin the time. 
+// Start the game and begin timer.
 startGameBtnEl.addEventListener("click", function () {
   // console.log("Button Clicked.");
   // startPageEl.style.display = "none";
@@ -13,12 +16,18 @@ startGameBtnEl.addEventListener("click", function () {
   renderQuestion();
 });
 
-
+// Function to render the question to the card.
 function renderQuestion() {
-  // Clear todoList element and update todoCountSpan
-  cardTitle.innerHTML = "Insert Function for Question Title";
-  cardText.textContent = "Insert Funtion for Question Answers";
-  startGameBtnEl.remove()
+  cardTitle.textContent = "";
+  cardText.textContent = "";
+  startGameBtnEl.remove();
+  cardTitle.textContent = question[0];
+
+  // Loop for answers in array still testing this 
+  for (i = 0; i < answers.length; i++) {
+    var answer = answers[i];
+    cardText.innerHTML += answers[i] + "<br>";
+  }
 }
 
 // function startQuiz(event) {
@@ -29,7 +38,7 @@ function renderQuestion() {
 //   }
 // }
 
-// Timer function 
+// Timer function
 function timeRemain() {
   var timeLeft = 60;
   var timeInterval = setInterval(function () {
