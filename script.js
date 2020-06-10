38cf1f0247ff11afe0f8f2d9656b24f6b190c09a
James Holding <james.holding90@gmail.com>
2020-06-09 21:53:06 -0400

    Function added to high display of start-page when button clicked.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+var startGameBtnEl = document.getElementById("start-button");
+var startPageEl = document.getElementById("start-page");
+
+startGameBtnEl.addEventListener("click", function (event) {
+  console.log("Button Clicked.");
+  if ("click") {
+    startPageEl.style.display = "none";
+  } else {
+    return;
+  }
+});
